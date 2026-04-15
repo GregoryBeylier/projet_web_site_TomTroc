d7cd206eb65c5097bb3656f8caa87f53bc8eafe9
Gregory BEYLIER <samsung261101@gmail.com>
2026-04-15 17:19:28 +0200

    bugfix

--- views/book/detail.php
+++ views/book/detail.php
@@ -9,14 +9,28 @@
 </head>
 
 <body>
-    <?php require __DIR__ . '/../templates/header.php'; ?>
-    <h1><?php echo $book->getTitle(); ?></h1>
-    <p><strong>Auteur :</strong> <?php echo $book->getAuthor(); ?></p>
-    <p><strong>Description :</strong> <?php echo $book->getDescription(); ?></p>
-    <img src="<?php echo $book->getPicture(); ?>" alt="couverture">
-    <p><a href="index.php?controller=user&action=showProfile&id=<?php echo $book->getUserId(); ?>">Propriétaire : <?php echo $user->getPseudo(); ?></a></p>
-    <button><a href="index.php?controller=message&action=thread&id=<?php echo $book->getUserId(); ?>">Envoyer un message</a></button>
-    <?php require __DIR__ . '/../templates/footer.php'; ?>
+    <div class="container_book_detail">
+        <div class="book_detail_left">
+            <img src="<?php echo $book->getPicture(); ?>" alt="couverture">
+        </div>
+
+        <div class="book_detail_right">
+            <h1><?php echo $book->getTitle(); ?></h1>
+            <p class="book_author">par <?php echo $book->getAuthor(); ?></p>
+            <hr>
+            <p class="book_description_label">DESCRIPTION</p>
+            <p class="book_description"><?php echo $book->getDescription(); ?></p>
+            <p class="book_owner_label">PROPRIÉTAIRE</p>
+            <div class="book_owner">
+                <img src="<?php echo $user->getProfilePhoto() ?: 'picture/default_profile.png'; ?>" alt="photo">
+                <a href="index.php?controller=user&action=showProfile&id=<?php echo $book->getUserId(); ?>">
+                    <?php echo $user->getPseudo(); ?>
+                </a>
+            </div>
+
+            <a href="index.php?controller=message&action=thread&id=<?php echo $book->getUserId(); ?>" class="btn_send_message">Envoyer un message</a>
+        </div>
+    </div>
 </body>
 
 </html>
